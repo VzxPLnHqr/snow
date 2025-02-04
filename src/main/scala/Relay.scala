4d66cce8ecb6bb61299c9a2b503d399e44d8e91b
VzxPLnHqr <VzxPLnHqr@proton.me>
2025-02-04 03:55:02 +0000

    RelayTest finally passing, including not hanging forever (tests against relay.damus.io).

--- src/main/scala/Relay.scala
+++ src/main/scala/Relay.scala
@@ -8,103 +8,135 @@ import fs2.concurrent.{Topic, Channel}
 import cats.implicits.*
 import cats.effect.*
 import org.http4s.Uri
-import org.http4s.dom.*
 import org.http4s.client.websocket.*
+import org.http4s.dom.*
 import io.circe.*
 import io.circe.parser.decode
 import io.circe.syntax.*
+import cats.Show
+
+/**
+  * An implementation of Relay[F] provides methods like `subscribe` 
+  * and access to the underlying streams of things while
+  * hiding the details about how it gets the things.
+  */
+trait Relay[F[_]]:
+  def uri: Uri
+  def nextId: Ref[F, Int]
+  def commands: Channel[F, Json]
+  def events: Topic[F, (String, Event)]
+  def eoses: Topic[F, String]
+  /**
+   * subscribing with a `Filter` will give us a list of stored events
+   * and a stream of future events */
+  def subscribe( filter: Filter*): F[(List[Event], Stream[F,Event])]
 
 object Relay {
-  def apply(uri: Uri): Resource[IO, Relay] = {
-    val normalized = uri
-      .withPath(uri.path.dropEndsWithSlash)
-      .withoutFragment
-      .toString()
 
-    (
-      Resource.eval(Ref[IO].of(0)),
-      Resource.eval(Channel.unbounded[IO, Json]),
-      Resource.eval(Topic[IO, (String, Event)]),
-      Resource.eval(Topic[IO, String]),
-      WebSocketClient[IO].connectHighLevel(WSRequest(uri))
-    ).mapN((nextId, commands, events, eoses, conn) =>
-      new Relay(uri, conn, nextId, commands, events, eoses)
-    ).flatTap(_.start.background)
-  }
+  /* this should ideally be for a generic F[_], but we hard code it to F = IO */
+  def apply(uri: Uri): Resource[IO, Relay[IO]] = mkResourceForIO(uri)
+
+  /* can make this more generic eventually, but for now it is tied to F = IO */
+  def mkResourceForIO(uri: Uri, debugOn: Boolean = false): Resource[IO, Relay[IO]] =
+
+    def debug[A](x: A)(using Show[A]) = if(debugOn) then IO.println(x) else IO.unit
+
+    for
+      nextId <- Ref[IO].of(0).toResource
+      commands <- Channel.unbounded[IO, Json].toResource
+      events <- Topic[IO, (String, Event)].toResource
+      eoses <- Topic[IO,String].toResource
+      conn <- WebSocketClient[IO].connectHighLevel(WSRequest(uri))
+
+      // here we weave together the websocket streams and start the background
+      // process that keeps them going
+      background <- {
+        val receive = debug("opening receive stream") *> conn.receiveStream
+          .collect { case WSFrame.Text(line, _) => line }
+          .map(line => decode[List[Json]](line.toString))
+          .collect { case Right(v) => v }
+          .evalTap[IO, Unit] { msg =>
+            msg match {
+              case msg if msg.size == 2 && msg(0).as[String] == Right("EOSE") =>
+                msg(1).as[String] match {
+                  case Right(subid) => debug(s"received eose for subid:$subid") 
+                                        *> eoses.publish1(subid).void 
+                                          *> debug(s"published eose for subid:$subid")
+                  case _            => IO.unit
+                }
+              case msg if msg.size == 3 && msg(0).as[String] == Right("EVENT") =>
+                (msg(1).as[String], msg(2).as[Event]) match {
+                  case (Right(subid), Right(event)) if event.isValid =>
+                    events.publish1((subid, event)).void
+                  case _ => IO.unit
+                }
+              case _ => IO.unit
+            }
+          }
+          .compile
+          .drain
+
+        val send = debug("opening send stream") *> commands.stream
+          .evalMap { msg => debug(s"sending request: $msg") *> conn.sendText(msg.noSpaces) }
+          .compile
+          .drain
+
+        (send, receive).parTupled.void.background
+      }
+      // only thing left to do now is return our Relay
+    yield new RelayImplForIO(uri, nextId, commands, events, eoses, debugOn)
 }
 
-class Relay(
-    uri: Uri,
-    conn: WSConnectionHighLevel[IO],
-    nextId: Ref[IO, Int],
-    commands: Channel[IO, Json],
-    events: Topic[IO, (String, Event)],
-    eoses: Topic[IO, String]
-) {
+class RelayImplForIO(
+    val uri: Uri,
+    val nextId: Ref[IO, Int],
+    val commands: Channel[IO, Json],
+    val events: Topic[IO, (String, Event)],
+    val eoses: Topic[IO, String],
+    debugOn: Boolean
+) extends Relay[IO]{
+
+  def debug[A](x: A)(using Show[A]) = if(debugOn) then IO.println(x.show) else IO.unit
+
   def subscribe(
       filter: Filter*
   ): IO[(List[Event], fs2.Stream[IO, Event])] = {
-    nextId.modify(x => (x + 1, x)).map(_.toString).flatMap { currId =>
-      val send = commands.send(
-        Seq("REQ".asJson, currId.asJson)
-          .concat(filter.map(_.asJson))
-          .asJson
-      )
+    debug(s"subscribing to filter $filter") *> 
+    nextId.getAndUpdate(_ + 1).map(_.toString).flatMap { 
+      currId =>
+        val send = commands.send(
+          Seq("REQ".asJson, currId.asJson)
+            .concat(filter.map(_.asJson))
+            .asJson
+        )
 
-      val eose =
-        eoses
-          .subscribe(1)
-          .collect {
-            case subid if subid == currId => Event(kind = -1, content = "")
-          }
-          .delayBy(700.milliseconds)
+        val eose =
+          eoses
+            .subscribe(1)
+            .collect {
+              case subid if subid == currId => Event(kind = -1, content = "")
+            }.evalTap(e => debug(s"eose received: $e"))
 
-      val receive =
-        events
-          .subscribe(1)
-          .collect {
-            case (subid, event) if subid == currId => event
-          }
-          .merge(eose)
+        val receive =
+          events
+            .subscribe(1)
+            .collect {
+              case (subid, event) if subid == currId => event
+            }
+            .merge(eose)
 
-      for {
-        _ <- send
-        stored <- receive.takeWhile(_.kind != -1).compile.toList
-        live = receive.dropWhile(_.kind != -1).drop(1)
-      } yield (stored, live)
+        // get the stored events. If we wanted to also keep the EOSE event
+        // we would use `takeThrough` here.
+        val stored = receive.takeWhile(_.kind != -1)
+            .compile.toList
+        
+        // note: it may seem like the live events should be something like
+        // `receive.drop(stored.size)`, but that would cause us to miss events.
+        // This is due to how receive is actually a subscription to the events Topic.
+        val live = receive
+        
+        // we make sure to trigger `send` first
+        send *> stored.map((_,live))
     }
-  }
-
-  def start: IO[Unit] = {
-    val receive =
-      conn.receiveStream
-        .collect { case WSFrame.Text(line, _) => line }
-        .map(line => decode[List[Json]](line.toString))
-        .collect { case Right(v) => v }
-        .evalTap[IO, Unit] { msg =>
-          msg match {
-            case msg if msg.size == 2 && msg(0).as[String] == Right("EOSE") =>
-              msg(1).as[String] match {
-                case Right(subid) => eoses.publish1(subid).void
-                case _            => IO.unit
-              }
-            case msg if msg.size == 3 && msg(0).as[String] == Right("EVENT") =>
-              (msg(1).as[String], msg(2).as[Event]) match {
-                case (Right(subid), Right(event)) if event.isValid =>
-                  events.publish1((subid, event)).void
-                case _ => IO.unit
-              }
-            case _ => IO.unit
-          }
-        }
-        .compile
-        .drain
-
-    val send = commands.stream
-      .evalTap { msg => conn.sendText(msg.noSpaces) }
-      .compile
-      .drain
-
-    (send, receive).parTupled.void
   }
 }
--- src/test/scala/RelayTest.scala
+++ src/test/scala/RelayTest.scala
@@ -8,35 +8,31 @@ import cats.effect.*
 import cats.effect.unsafe.implicits.global
 import org.http4s.syntax.literals.uri
 import fs2.concurrent.Channel
-/*
-object RelayTest extends TestSuite {
-  scalajs.js.Dynamic.global.globalThis.require("websocket-polyfill")
 
+object RelayTest extends TestSuite {
   val tests = Tests {
     test("connect to relay and subscribe") {
-      val program = Relay(uri"wss://nostr.fmt.wiz.biz").use { relay =>
-        relay
-          .subscribe(
-            Filter(kinds = List(1), limit = Some(5))
-          )
-          .flatMap { (stored, live) =>
-            IO.delay {
-              println(stored.size)
-              assert(stored.size == 5)
-            } *>
-              live
-                .evalTap { evt =>
-                  IO.delay {
-                    assert(evt.kind == 0)
-                  }
-                }
-                .take(1)
-                .compile
-                .drain
-          }
-      }
+      val numStoredEvents = 3
+      val program = Relay.mkResourceForIO(uri"wss://relay.damus.io", debugOn = true)
+        .use { relay => 
+          relay
+            .subscribe(
+              Filter(kinds = List(1), limit = Some(numStoredEvents))
+            )
+            .flatMap { (stored, live) =>
+              stored.traverse(e => IO.println((e.kind, e.hash))) *> IO.println(s"done processing ${stored.size} stored events") *>
+              IO.delay {
+                assert(stored.size == numStoredEvents)
+              } *> IO.println("now processing live stream of events (stopping after 5)") *>
+                live
+                  .take(5)
+                  .evalTap(e => IO.println((e.kind,e.hash)))
+                  .compile
+                  .drain
+            }
+        }
 
       program.unsafeToFuture()
     }
   }
-}*/
+}
